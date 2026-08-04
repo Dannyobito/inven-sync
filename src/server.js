935648a8fc525b7a187c7153df2e98968c1dce7e
Dannyobito <dannyobito05@gmail.com>
2026-08-04 21:32:25 +0100

    dockerize api and postgres

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const Hapi = require('@hapi/hapi');
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 5005,
-    host: '0.0.0.0',
+    host: process.env.HOST || '0.0.0.0',
   });
 
   server.route({
@@ -17,7 +17,9 @@ const init = async () => {
   });
 
   await server.start();
-  console.log(`Server running on ${server.info.uri}`);
+  const displayHost =
+    server.info.host === '0.0.0.0' ? 'localhost' : server.info.host;
+  console.log(`Server running on http://${displayHost}:${server.info.port}`);
 };
 
 process.on('unhandledRejection', (err) => {
